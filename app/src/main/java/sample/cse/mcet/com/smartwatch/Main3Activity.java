aaab2adc75f4a043309aabecbc2f5729ffe5a62e
dhivyanand <dhivyananthan98@gmail.com>
2018-09-06 14:24:01 +0530

    Initial Commit

---
+++ app/src/main/java/sample/cse/mcet/com/smartwatch/Main3Activity.java
@@ -0,0 +1,22 @@
+package sample.cse.mcet.com.smartwatch;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main3Activity extends AppCompatActivity {
+
+    public void onClick2(View view){
+
+        Intent intent=new Intent(this,LoginActivity.class);
+        startActivity(intent);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
